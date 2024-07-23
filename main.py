e231846fee657960cc13102c912962b94218c04a
abderraouf-metidji <115145685+abderraouf-metidji@users.noreply.github.com>
2024-07-23 09:58:10 +0200

    update with multithreading

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from bdd import get_database
 from scrap_beautifulsoup import get_questions as get_questions_bs
 from scrap_selenium import get_questions as get_questions_selenium, setup_driver
+from multiprocessing.pool import ThreadPool
 
 """
 Méthode pour scraper les données et les insérer dans la base de données
@@ -15,6 +16,7 @@ def scrap_data():
         return
     
     number_of_pages = int(input("How many pages do you want to scrape? "))
+    
     """
     On récupère les questions
     """
@@ -24,6 +26,23 @@ def scrap_data():
         driver.quit()
     else:
         questions = get_questions_bs(number_of_pages)
+        
+    """"
+    Boucle de multithreading pour scraper les questions en parallèle
+    """
+    if method == "2":
+        with ThreadPool(10) as pool:
+            questions_list = pool.map(get_questions_bs, range(1, number_of_pages + 1))
+            pool.close()
+    else:
+        with ThreadPool(10) as pool:
+            questions_list = pool.map(get_questions_selenium, range(1, number_of_pages + 1))
+            pool.close()
+            
+    """
+    Flatten the list of lists
+    """
+    questions = [question for sublist in questions_list for question in sublist]
 
     """
     On se connecte à la base de données
--- test_requete.py
+++ test_requete.py
@@ -1,25 +1,21 @@
-import unittest
+import pytest
 from unittest.mock import patch
+import requests
 from scrap_beautifulsoup import fetch_page
 
 
-class TestFetchPage(unittest.TestCase):
+@patch('scrap_beautifulsoup.requests.get')
+def test_fetch_page_success(mock_get):
     """
-    Une classe de test pour tester la fonction fetch_page.
+    Teste la fonction fetch_page pour une requête réussie.
     """
-
-    @patch('scrap_beautifulsoup.requests.get')
-    def test_fetch_page_success(self, mock_get):
-        """
-        Teste la fonction fetch_page pour une requête réussie.
-        """
-        mock_get.return_value.status_code = 200
-        mock_get.return_value.content = b"Contenu de la page"
-        
-        url = "https://stackoverflow.com"
-        result = fetch_page(url)
-        
-        self.assertEqual(result, b"Contenu de la page")
+    mock_get.return_value.status_code = 200
+    mock_get.return_value.content = b"Contenu de la page"
+    
+    url = "https://stackoverflow.com"
+    result = fetch_page(url)
+    
+    assert result == b"Contenu de la page"
 
 if __name__ == '__main__':
-    unittest.main()
+    pytest.main()
